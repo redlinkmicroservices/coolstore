fba1f8af20bbd3599f705369c950f21db906016b
Zach Gutterman <zgutterm@redhat.com>
2018-02-13 16:08:14 -0500

    adding gateway solution

---
+++ coolstore-gateway/src/main/java/com/redhat/coolstore/api/gateway/model/Product.java
@@ -0,0 +1,55 @@
+package com.redhat.coolstore.api.gateway.model;
+
+public class Product {
+
+    public String itemId;
+
+    public String name;
+
+    public String desc;
+
+    public double price;
+
+    public Inventory availability;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Inventory getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Inventory availability) {
+        this.availability = availability;
+    }
+
+}

94e569ce45c6a0a1838389d8a7ade67f6ef36eed
Zach Gutterman <zgutterm@redhat.com>
2018-02-20 15:37:55 -0500

    add hystrix to gateway

--- coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/CartGateway.java
+++ coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/CartGateway.java
@@ -9,62 +9,111 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
 import com.redhat.coolstore.gateway.model.ShoppingCart;
 import com.redhat.coolstore.gateway.proxy.CartResource;
 
-
+@SuppressWarnings("cdi-ambiguous-dependency")
 @Path("cart")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @RequestScoped
 public class CartGateway {
-	
-	@SuppressWarnings("cdi-ambiguous-dependency")
+
 	@Inject
-	@ConfigProperty(name="CART_SERVICE_URL", defaultValue="")
+	@ConfigProperty(name = "CART_SERVICE_URL", defaultValue = "")
 	private String cartURL;
-	
+
+	@Inject
+	@ConfigurationValue("hystrix.cart.executionTimeout")
+	private int hystrixExecutionTimeout;
+
+	@Inject
+	@ConfigurationValue("hystrix.cart.groupKey")
+	private String hystrixGroupKey;
+
+	@Inject
+	@ConfigurationValue("hystrix.cart.circuitBreakerEnabled")
+	private boolean hystrixCircuitBreakerEnabled;
+
 	private CartResource buildClient() {
-		System.out.println("Building new client " + cartURL);
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(cartURL);
-		ResteasyWebTarget restEasyTarget = (ResteasyWebTarget)target;
-		return restEasyTarget.proxy(CartResource.class);
-		
+		try {
+			CartResource cartResource = new GetWebTarget(client).execute();
+			if (cartResource == null) {
+				throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
+			} else {
+				return cartResource;
+			}
+
+		}  catch (HystrixRuntimeException e) {
+			throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
+		}
+
 	}
-	
+
 	@GET
 	@Path("/{cartId}")
 	public ShoppingCart getCart(@PathParam("cartId") String cartId) {
 		CartResource proxy = buildClient();
 		return proxy.getCart(cartId);
 	}
-	
+
 	@POST
 	@Path("/{cartId}/{itemId}/{quantity}")
-	public ShoppingCart addToCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId, @PathParam("quantity") int quantity) {
+	public ShoppingCart addToCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId,
+			@PathParam("quantity") int quantity) {
 		CartResource proxy = buildClient();
 		return proxy.addToCart(cartId, itemId, quantity);
 	}
 
 	@DELETE
 	@Path("/{cartId}/{itemId}/{quantity}")
-	public ShoppingCart removeFromCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId, @PathParam("quantity") int quantity) {
+	public ShoppingCart removeFromCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId,
+			@PathParam("quantity") int quantity) {
 		CartResource proxy = buildClient();
 		return proxy.removeFromCart(cartId, itemId, quantity);
 	}
+
 	@POST
 	@Path("/checkout/{cartId}")
-	public ShoppingCart checkout(@PathParam("cartId")  String cartId) {
+	public ShoppingCart checkout(@PathParam("cartId") String cartId) {
 		CartResource proxy = buildClient();
 		return proxy.checkout(cartId);
 	}
+
+	public class GetWebTarget extends HystrixCommand<CartResource> {
+		private Client client;
+
+		public GetWebTarget(Client client) {
+			super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(hystrixGroupKey))
+					.andCommandPropertiesDefaults(
+							HystrixCommandProperties.Setter().withCircuitBreakerEnabled(hystrixCircuitBreakerEnabled)
+									.withExecutionTimeoutInMilliseconds(hystrixExecutionTimeout)));
+			this.client = client;
+
+		}
+
+		@Override
+		protected CartResource run() throws Exception {
+			WebTarget target = client.target(cartURL);
+			ResteasyWebTarget restEasyTarget = (ResteasyWebTarget) target;
+			return restEasyTarget.proxy(CartResource.class);
+		}
+	}
+
 }
--- coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/ProductGateway.java
+++ coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/ProductGateway.java
@@ -3,45 +3,85 @@ package com.redhat.coolstore.gateway.api;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixCommand.Setter;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import com.redhat.coolstore.gateway.api.CartGateway.GetWebTarget;
+import com.redhat.coolstore.gateway.model.Inventory;
 import com.redhat.coolstore.gateway.model.Product;
+import com.redhat.coolstore.gateway.proxy.CartResource;
 import com.redhat.coolstore.gateway.proxy.ProductResource;
-
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@SuppressWarnings("cdi-ambiguous-dependency")
 @RequestScoped
 public class ProductGateway {
-	
+
 	@Inject
-	@ConfigProperty(name="CATALOG_SERVICE_URL", defaultValue="")
+	@ConfigProperty(name = "CATALOG_SERVICE_URL", defaultValue = "")
 	private String catalogServiceUrl;
-	
-	
+
+	@Inject
+	@ConfigurationValue("hystrix.products.executionTimeout")
+	private int hystrixProductsExecutionTimeout;
+
+	@Inject
+	@ConfigurationValue("hystrix.products.groupKey")
+	private String hystrixProductsGroupKey;
+
+	@Inject
+	@ConfigurationValue("hystrix.products.circuitBreakerEnabled")
+	private boolean hystrixProductsCircuitBreakerEnabled;
+
+	@Inject
+	@ConfigurationValue("hystrix.inventory.executionTimeout")
+	private int hystrixInventoryExecutionTimeout;
+
+	@Inject
+	@ConfigurationValue("hystrix.inventory.groupKey")
+	private String hystrixInventoryGroupKey;
+
+	@Inject
+	@ConfigurationValue("hystrix.inventory.circuitBreakerEnabled")
+	private boolean hystrixInventoryCircuitBreakerEnabled;
+
 	private ProductResource buildClient() {
-		System.out.println("Building new client with uri " + catalogServiceUrl);
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(catalogServiceUrl);
-		ResteasyWebTarget restEasyTarget = (ResteasyWebTarget)target;
-		return restEasyTarget.proxy(ProductResource.class);
-		
+		try {
+			ProductResource productResource = new GetWebTarget(client).execute();
+			if (productResource == null) {
+				throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
+			} else {
+				return productResource;
+			}
+
+		}  catch (HystrixRuntimeException e) {
+			throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
+		}
+
 	}
-	
 
 	@GET
 	@Path("/products")
@@ -49,7 +89,7 @@ public class ProductGateway {
 		ProductResource proxy = buildClient();
 		return proxy.getProducts();
 	}
-	
+
 	@GET
 	@Path("/product/{itemId}")
 	public Product getProduct(@PathParam("itemId") String itemId) {
@@ -62,6 +102,45 @@ public class ProductGateway {
 	public void addProduct(final Product product) {
 		ProductResource proxy = buildClient();
 		proxy.addProduct(product);
+	}
+
+//	private Product getFallbackProduct() {
+//		Product product = new Product();
+//		product.setItemId("0");
+//		product.setName("Unavailable Product");
+//		product.setDesc("Unavailable Product");
+//		product.setPrice(0);
+//		product.setAvailability(getFallbackInventory());
+//		return product;
+//	}
+//
+//	private Inventory getFallbackInventory() {
+//		Inventory inventory = new Inventory();
+//		inventory.setItemId("0");
+//		inventory.setQuantity(0);
+//		inventory.setLocation("Local Store");
+//		inventory.setLink("http://developers.redhat.com");
+//		return inventory;
+//	}
+	
+	public class GetWebTarget extends HystrixCommand<ProductResource> {
+		private Client client;
+
+		public GetWebTarget(Client client) {
+			super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(hystrixProductsGroupKey))
+					.andCommandPropertiesDefaults(
+							HystrixCommandProperties.Setter().withCircuitBreakerEnabled(hystrixProductsCircuitBreakerEnabled)
+									.withExecutionTimeoutInMilliseconds(hystrixProductsExecutionTimeout)));
+			this.client = client;
+
+		}
+
+		@Override
+		protected ProductResource run() throws Exception {
+			WebTarget target = client.target(catalogServiceUrl);
+			ResteasyWebTarget restEasyTarget = (ResteasyWebTarget) target;
+			return restEasyTarget.proxy(ProductResource.class);
+		}
 	}
 
 }

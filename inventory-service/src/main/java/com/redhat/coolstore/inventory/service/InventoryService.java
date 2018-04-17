fd8ba1afb82780738f90fb08250a8b4628b7b2d8
Ravi Srinivasan <rsriniva@users.noreply.github.com>
2018-04-17 13:48:18 +0530

    Added TODOs for JBDS hinting

--- inventory-service/src/main/java/com/redhat/coolstore/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/redhat/coolstore/inventory/service/InventoryService.java
@@ -3,15 +3,14 @@ package com.redhat.coolstore.inventory.service;
 import javax.persistence.EntityManager;
 import com.redhat.coolstore.inventory.model.Inventory;
 
-// Add appropriate scope for this class
+// TODO: Add appropriate scope for this class
 public class InventoryService {
 
-    // Inject the "primary" persistence context
+    // TODO: Inject the "primary" persistence context
     private EntityManager em;
 
     public Inventory getInventory(String itemId) {
-        // Use the entity manager to look up a single inventory item
-	// based on item id, and then return it.
+        // TODO: Use the entity manager to look up a single inventory item based on item id, and then return it.
         return null;
     }
 

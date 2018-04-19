85a7c6f5e1f879a1ef318af634a00bad716573c4
Ravi Srinivasan <rsriniva@users.noreply.github.com>
2018-04-19 18:32:37 +0530

    Added JBDS TODO markers

--- inventory-service/src/main/java/com/redhat/coolstore/inventory/rest/HealthCheckResource.java
+++ inventory-service/src/main/java/com/redhat/coolstore/inventory/rest/HealthCheckResource.java
@@ -1,18 +1,15 @@
 package com.redhat.coolstore.inventory.rest;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import org.wildfly.swarm.health.Health;
 import org.wildfly.swarm.health.HealthStatus;
 
 @Path("/")
 public class HealthCheckResource {
 
-    // Add annotations to make this method
-    // provide health status for this service at the path /status
+    // TODO: Add annotations to make this method provide health status for this service at the path /status
     public HealthStatus check() {
-	// Return the health status as "UP"
+	// TODO: Return the health status as "UP"
 	return null;
     }
 

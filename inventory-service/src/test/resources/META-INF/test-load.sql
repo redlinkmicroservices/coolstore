dd8af7a36a81f63f26e85760bdd728ff69032cba
Fernando Lozano <flozano@redhat.com>
2018-05-16 16:06:35 -0300

    removed projects unused during ch04

--- inventory-service/src/test/resources/META-INF/test-load.sql
+++
@@ -1,2 +0,0 @@
-insert into PRODUCT_INVENTORY (itemId, link, location, quantity) values ('123456', 'link', 'location', 99);
-insert into PRODUCT_INVENTORY (itemId, link, location, quantity) values ('123457', 'www.go.com', 'Bangalore', 20);

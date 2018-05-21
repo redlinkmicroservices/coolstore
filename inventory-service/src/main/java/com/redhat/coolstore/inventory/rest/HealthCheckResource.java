e232c5b815eb45227ca08af3b1fb55fd4669a584
Ravi Srinivasan <rsriniva@redhat.com>
2018-05-21 17:50:34 +0530

    Providing more complete health check resource class

--- inventory-service/src/main/java/com/redhat/coolstore/inventory/rest/HealthCheckResource.java
+++ inventory-service/src/main/java/com/redhat/coolstore/inventory/rest/HealthCheckResource.java
@@ -1,5 +1,6 @@
 package com.redhat.coolstore.inventory.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import org.wildfly.swarm.health.HealthStatus;
@@ -7,10 +8,12 @@ import org.wildfly.swarm.health.HealthStatus;
 @Path("/")
 public class HealthCheckResource {
 
-    // TODO: Add annotations to make this method provide health status for this service at the path /status
+	@GET
+	@Path("/status")
+	// TODO: Add annotations to make this method provide health status for this service
     public HealthStatus check() {
-	// TODO: Return the health status as "UP"
-	return null;
+	    // TODO: Return the health status as "UP"
+	    return null;
     }
 
 }

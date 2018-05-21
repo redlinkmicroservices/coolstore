e4c44659165c1e5aa4c385709548dea6dd9eca20
Fernando Lozano <flozano@redhat.com>
2018-05-21 11:26:11 -0300

    Removed health endopoint implementation

--- cart-service/src/main/java/com/redhat/coolstore/cart/rest/HealthCheckEndpoint.java
+++ cart-service/src/main/java/com/redhat/coolstore/cart/rest/HealthCheckEndpoint.java
@@ -6,20 +6,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.HealthEndpoint;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
 
 @Path("/")
 @Component
 public class HealthCheckEndpoint {
 
-	@Autowired
-	HealthEndpoint healthEndpoint;
+	//TODO inject a HealthEndpoint
+
 	@GET
 	@Path("/health")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Health getHealth() {
-		return healthEndpoint.invoke();
+	//TODO change the return type
+	public String getHealth() {
+		//TODO invoke the HeanthEndpoint and return the result Health object
+		return null;
 	}
 }

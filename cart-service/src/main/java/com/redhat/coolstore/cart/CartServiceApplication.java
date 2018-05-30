55468cc234ba2d5c2785f865537e0d0761de7b30
Ricardo Jun <jtaniguc@redhat.com>
2018-05-30 13:58:13 -0300

    Fixed dependency issue

--- cart-service/src/main/java/com/redhat/coolstore/cart/CartServiceApplication.java
+++ cart-service/src/main/java/com/redhat/coolstore/cart/CartServiceApplication.java
@@ -2,8 +2,12 @@ package com.redhat.coolstore.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrix
 public class CartServiceApplication {
 
     public static void main(String[] args) {

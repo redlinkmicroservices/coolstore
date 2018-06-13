90af5878637ba54ca7aa43dff7096b09fe0a8461
Ricardo Jun <jtaniguc@redhat.com>
2018-06-13 14:55:25 -0300

    Included comments to code the Hystrix command support

--- coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/CartGateway.java
+++ coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/CartGateway.java
@@ -17,10 +17,6 @@ import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
-import com.netflix.hystrix.HystrixCommand;
-import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixCommandKey;
-import com.netflix.hystrix.HystrixCommandProperties;
 import com.redhat.coolstore.gateway.model.ShoppingCart;
 import com.redhat.coolstore.gateway.proxy.CartResource;
 
@@ -46,145 +42,48 @@ public class CartGateway {
 	@Path("/{cartId}")
 	public ShoppingCart getCart(@PathParam("cartId") String cartId) {
 		CartResource proxy = buildClient();
-		return new GetCartCommand(proxy,cartId).execute();
+		//TODO use the HystrixCommand created for the getCart method and invoke the execute method 
+		return proxy.getCart(cartId);
 	}
 
-	public static class GetCartCommand extends HystrixCommand<ShoppingCart>{
-		public final static String GET_CART_COMMAND_KEY="GetCartCommandKey";
-		private CartResource proxy;
-		private String cartId;
+//TODO Create a class that extends a HystrixCommand for the getCart method named GetCartCommand
 
-		public GetCartCommand(CartResource proxy, String cartId) {
-			super(Setter
-					.withGroupKey(HystrixCommandGroupKey.Factory.asKey("group"))
-					.andCommandKey(HystrixCommandKey.Factory.asKey(GET_CART_COMMAND_KEY))
-					.andCommandPropertiesDefaults(
-							HystrixCommandProperties
-								.Setter()
-									.withCircuitBreakerRequestVolumeThreshold(2)
-									.withCircuitBreakerSleepWindowInMilliseconds(5000)));
-			this.cartId=cartId;
-			this.proxy=proxy;
-		}
-
-		@Override
-		protected ShoppingCart run() throws Exception {
-			return proxy.getCart(cartId);
-		}
-		
-	}
 	
 	@POST
 	@Path("/{cartId}/{itemId}/{quantity}")
 	public ShoppingCart addToCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId,
 			@PathParam("quantity") int quantity) {
 		CartResource proxy = buildClient();
-		return new AddToCartCommand(proxy, cartId, itemId, quantity).execute();
+		//TODO use the HystrixCommand created for the addToCart method and invoke the execute method 
+		return proxy.addToCart(cartId, itemId, quantity);
 	}
 	
-	public static class AddToCartCommand extends HystrixCommand<ShoppingCart>{
-
-		private CartResource proxy;
-		private String cartId;
-		private String itemId;
-		private int quantity;
-		public final static String ADD_TO_CART_COMMAND_KEY="AddToCartCommandKey";
-		
-		public AddToCartCommand(CartResource proxy, String cartId, String itemId, int quantity) {
-			super(Setter
-					.withGroupKey(HystrixCommandGroupKey.Factory.asKey("group")).andCommandKey(HystrixCommandKey.Factory.asKey(ADD_TO_CART_COMMAND_KEY))
-					.andCommandPropertiesDefaults(
-							HystrixCommandProperties
-								.Setter()
-									.withCircuitBreakerRequestVolumeThreshold(2)
-									.withCircuitBreakerSleepWindowInMilliseconds(5000)));
-			this.cartId=cartId;
-			this.itemId=itemId;
-			this.quantity=quantity;
-			this.proxy=proxy;
-			
-		}
-
-		@Override
-		protected ShoppingCart run() throws Exception {
-			return proxy.addToCart(cartId, itemId, quantity);
-		}
-		
-	}
+	//TODO Create a class that extends a HystrixCommand for the addToCart method named AddToCartCommand
 
 	@DELETE
 	@Path("/{cartId}/{itemId}/{quantity}")
 	public ShoppingCart removeFromCart(@PathParam("cartId") String cartId, @PathParam("itemId") String itemId,
 			@PathParam("quantity") int quantity) {
 		CartResource proxy = buildClient();
-		return new RemoveFromCartCommand(proxy, cartId, itemId, quantity).execute();
+		//TODO use the HystrixCommand created for the removeFromCart method and invoke the execute method 
+
+		return proxy.removeFromCart(cartId, itemId, quantity);
 	}
 
-	public static class RemoveFromCartCommand extends HystrixCommand<ShoppingCart>{
+	//TODO Create a class that extends a HystrixCommand for the removeFromCart method named RemoveFromCartCommand
 
-		private CartResource proxy;
-		private String cartId;
-		private String itemId;
-		private int quantity;
-		public static final String REMOVE_CART_COMMAND_KEY="RemoveFromCartCommandKey";
-		
-		public RemoveFromCartCommand(CartResource proxy, String cartId, String itemId, int quantity) {
-			super(Setter
-					.withGroupKey(HystrixCommandGroupKey.Factory.asKey("group"))
-					.andCommandKey(HystrixCommandKey.Factory.asKey(REMOVE_CART_COMMAND_KEY))
-					.andCommandPropertiesDefaults(
-							HystrixCommandProperties
-								.Setter()
-									.withCircuitBreakerRequestVolumeThreshold(2)
-									.withCircuitBreakerSleepWindowInMilliseconds(5000)));
-			this.cartId=cartId;
-			this.itemId=itemId;
-			this.quantity=quantity;
-			this.proxy=proxy;
-			
-		}
-
-		@Override
-		protected ShoppingCart run() throws Exception {
-			return proxy.removeFromCart(cartId, itemId, quantity);
-		}
-		
-	}
 
 	
 	@POST
 	@Path("/checkout/{cartId}")
 	public ShoppingCart checkout(@PathParam("cartId") String cartId) {
 		CartResource proxy = buildClient();
-		return new CheckoutCommand(proxy, cartId).execute();
+		//TODO use the HystrixCommand created for the removeFromCart method and invoke the execute method 
+		return proxy.checkout(cartId);
 	}
 	
-	public static class CheckoutCommand extends HystrixCommand<ShoppingCart>{
+	//TODO Create a class that extends a HystrixCommand for the checkout method named CheckoutCommand
 
-		private CartResource proxy;
-		private String cartId;
-		public static final String CHECKOUT_CART_COMMAND_KEY="CheckoutCartCommandKey";
-		
-		public CheckoutCommand(CartResource proxy, String cartId) {
-			super(Setter
-					.withGroupKey(HystrixCommandGroupKey.Factory.asKey("group"))
-					.andCommandKey(HystrixCommandKey.Factory.asKey(CHECKOUT_CART_COMMAND_KEY))
-					.andCommandPropertiesDefaults(
-							HystrixCommandProperties
-								.Setter()
-									.withCircuitBreakerRequestVolumeThreshold(2)
-									.withCircuitBreakerSleepWindowInMilliseconds(5000)));
-			this.cartId=cartId;
-			this.proxy=proxy;
-			
-		}
-
-		@Override
-		protected ShoppingCart run() throws Exception {
-			return proxy.checkout(cartId);
-		}
-		
-	}
-
+	
 
 }
--- coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/ProductGateway.java
+++ coolstore-gateway-jaxrs/src/main/java/com/redhat/coolstore/gateway/api/ProductGateway.java
@@ -10,72 +10,33 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
-import com.netflix.hystrix.HystrixCommand;
-import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixCommandProperties;
-import com.netflix.hystrix.exception.HystrixRuntimeException;
 import com.redhat.coolstore.gateway.model.Product;
 import com.redhat.coolstore.gateway.proxy.ProductResource;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@SuppressWarnings("cdi-ambiguous-dependency")
 @RequestScoped
 public class ProductGateway {
 
 	@Inject
-	@ConfigProperty(name = "CATALOG_SERVICE_URL", defaultValue = "http://catalog-service-coolstore-catalog.192.168.99.100.nip.io:80")
+	@ConfigProperty(name = "CATALOG_SERVICE_URL")
 	private String catalogServiceUrl;
 
-	@Inject
-	@ConfigProperty(name = "hystrix.products.executionTimeout", defaultValue="2000")
-	private int hystrixProductsExecutionTimeout;
-
-	@Inject
-	@ConfigProperty(name = "hystrix.products.groupKey", defaultValue ="product")
-	private String hystrixProductsGroupKey;
-
-	@Inject
-	@ConfigProperty(name = "hystrix.products.circuitBreakerEnabled", defaultValue="true")
-	private boolean hystrixProductsCircuitBreakerEnabled;
-
-	@Inject
-	@ConfigProperty(name = "hystrix.inventory.executionTimeout", defaultValue="1000")
-	private int hystrixInventoryExecutionTimeout;
-
-	@Inject
-	@ConfigProperty(name = "hystrix.inventory.groupKey", defaultValue="inventory")
-	private String hystrixInventoryGroupKey;
-
-	@Inject
-	@ConfigProperty(name = "hystrix.inventory.circuitBreakerEnabled", defaultValue="true")
-	private boolean hystrixInventoryCircuitBreakerEnabled;
 
 	private ProductResource buildClient() {
 		Client client = ClientBuilder.newClient();
-		try {
-			ProductResource productResource = new GetWebTarget(client).execute();
-			if (productResource == null) {
-				throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
-			} else {
-				return productResource;
-			}
-
-		}  catch (HystrixRuntimeException e) {
-			throw new WebApplicationException(Response.Status.SERVICE_UNAVAILABLE);
-		}
-
+		WebTarget target = client.target(catalogServiceUrl);
+		ResteasyWebTarget restEasyTarget = (ResteasyWebTarget) target;
+		return restEasyTarget.proxy(ProductResource.class);
 	}
 
 	@GET
@@ -100,24 +61,6 @@ public class ProductGateway {
 	}
 
 	
-	public class GetWebTarget extends HystrixCommand<ProductResource> {
-		private Client client;
 
-		public GetWebTarget(Client client) {
-			super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(hystrixProductsGroupKey))
-					.andCommandPropertiesDefaults(
-							HystrixCommandProperties.Setter().withCircuitBreakerEnabled(hystrixProductsCircuitBreakerEnabled)
-									.withExecutionTimeoutInMilliseconds(hystrixProductsExecutionTimeout)));
-			this.client = client;
-
-		}
-
-		@Override
-		protected ProductResource run() throws Exception {
-			WebTarget target = client.target(catalogServiceUrl);
-			ResteasyWebTarget restEasyTarget = (ResteasyWebTarget) target;
-			return restEasyTarget.proxy(ProductResource.class);
-		}
-	}
 
 }
